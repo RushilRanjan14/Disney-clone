576fd1960775fc4791a5a4ff5bf0015b4d291840
Rushil Ranjan <rushil1417@gmail.com>
2021-11-07 11:53:05 +0530

    no changes made

--- my-app/src/components/Disney.js
+++ my-app/src/components/Disney.js
@@ -33,20 +33,18 @@ const Container = styled.div`
 const Content = styled.div`
   display: grid;
   grid-gap: 25px;
-  gap: 25px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
+
 
 `;
 
 const Wrap = styled.div`
   padding-top: 56.25%;
   border-radius: 10px;
-  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
   cursor: pointer;
   overflow: hidden;
   position: relative;
-  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  transition: all 250ms;
   border: 3px solid rgba(249, 249, 249, 0.1);
 
   img {
@@ -56,15 +54,14 @@ const Wrap = styled.div`
     object-fit: cover;
     opacity: 1;
     position: absolute;
-    transition: opacity 500ms ease-in-out 0s;
+    transition: opacity 500ms ;
     width: 100%;
     z-index: 1;
     top: 0;
   }
 
   &:hover {
-    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
-      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+   
     transform: scale(1.05);
     border-color: rgba(249, 249, 249, 0.8);
   }
